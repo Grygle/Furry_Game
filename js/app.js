ecc924742c583a9c4b11cf19ecae1bb1a3fa1e58
Bezsilny <julek200@gmail.com>
2018-08-01 16:57:46 +0200

    interval moves

--- js/app.js
+++ js/app.js
@@ -18,29 +18,47 @@ function Game() {
     this.coin = new Coin();
     this.score = 0;
 
+    var _this = this;
 
 
     this.index1 = function(x,y) {
         return x + (y * 10);
     }
+
     this.showFurry = function() {
         this.board[ this.index1(this.furry.x,this.furry.y) ].classList.add('furry');
 
     }
+
     this.showCoin = function () {
         this.board[ this.index1(this.coin.x, this.coin.y) ].classList.add('coin');
     }
 
+    this.hideVisibleFurry = function () {
+        var toDelete = document.querySelector('.furry');
+        toDelete.classList.remove('furry');
+    }
+
     this.startGame = function () {
         this.idSetInterval = setInterval(function () {
-           console.log('hura set interval')
+            _this.moveFurry();
         }, 250);
     }
+
+    this.moveFurry = function () {
+        this.hideVisibleFurry();
+        if(this.furry.direction === 'right'){
+            this.furry.x += 1;
+        } else if(this.furry.direction === 'left'){
+            this.furry.x -= 1;
+        } else if(this.furry.direction === 'up') {
+            this.furry.y += 1;
+        } else {
+            this.furry.y -= 1;
+        }
+        this.showFurry();
+    }
 }
-
-
-
-
 
 
 document.addEventListener("DOMContentLoaded", function() {

2193f501e11a5ad042e199f49d951057403f666b
Bezsilny <julek200@gmail.com>
2018-08-01 13:14:16 +0200

    first 8 points are ready

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-console.log('whatup');
+function Furry() {
+    this.x = 0;
+    this.y = 0;
+    this.direction = 'right';
+}
+
+function Coin() {
+    this.x = Math.floor(Math.random() * 10);
+    this.y = Math.floor(Math.random() * 10);
+}
+
+function Game() {
+
+
+
+    this.board = document.querySelectorAll('#board div');
+    this.furry = new Furry();
+    this.coin = new Coin();
+    this.score = 0;
+
+
+
+    this.index1 = function(x,y) {
+        return x + (y * 10);
+    }
+    this.showFurry = function() {
+        this.board[ this.index1(this.furry.x,this.furry.y) ].classList.add('furry');
+
+    }
+    this.showCoin = function () {
+        this.board[ this.index1(this.coin.x, this.coin.y) ].classList.add('coin');
+    }
+
+    this.startGame = function () {
+        this.idSetInterval = setInterval(function () {
+           console.log('hura set interval')
+        }, 250);
+    }
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var game = new Game();
+    game.showFurry();
+    game.showCoin();
+    game.startGame();
+
+});
